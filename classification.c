372216d783c63cf758c198c463848d1aa4782ece
slashlyu <estellemarin26@gmail.com>
2017-02-03 11:18:36 +0100

    ajout

--- classification.c
+++ classification.c
@@ -1,1 +1,64 @@
-#include 
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+bool lecture_chaine(FILE *f, char tab[7]){
+		
+	while(c !=' ' || c!=':' || c!=EOF){
+		
+			
+			
+	
+
+
+int main(int argc, char *argv[]){
+	FILE *f;
+	char chaine[7];
+	char c;
+	bool
+	
+
+		f=fopen(argv[i+1],"r");
+		if(f== NULL) {
+			printf("Erreur ouverture fichier\n");
+			return 1;
+		}
+
+			c=getc(f);
+			while(b){
+
+				switch(e){
+					
+				case 0 : //lecture classe
+					
+				
+				}else{
+					mot++;
+				}
+
+				c=getc(f);
+			}
+			printf("%s", argv[i+1]);
+			printf("\n");
+			for(int j=0;j<26;j++)comptage[i][j]=((comptage[i][j])/(double)(lettre+NBLETTRE));
+			for(int j=0;j<26;j++)printf("Nombre de %c : %f\n",j+'a',comptage[i][j]);
+		/*for(int i=0;i<26;i++){
+			for(int j=0;j<26;j++){
+				printf("Nombre de %c précédé de %c : %d\n ",i+'a', j+ 'a', Bigramme[i][j]);
+			}
+		}*/
+		//printf("\t");
+		/*/for(int i=0; i<26; i++) {
+			printf("%c\t", i+'a');
+		}
+		for(int i=0;i<26;i++){
+			printf("\n%c \t", i+'a');
+			for(int j=0;j<26;j++){
+				printf("%d\t", Bigramme[i][j]);
+			}
+		}*/
+			printf("\n");printf("\n");printf("\n");
+			fclose(f);
+		}
+		return 0;
+}
